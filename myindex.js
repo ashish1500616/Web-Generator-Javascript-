7332ff8c39a4594a5b6a7de81cb36893a4bdd6bc
Ashish verma <ashish1500616@gmail.com>
2017-06-09 12:49:48 +0530

    solve mix content

--- myindex.js
+++ myindex.js
@@ -39,7 +39,7 @@
      var strTemp = $("#searchfield").val();
 
      console.log(strTemp);
-     $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + strTemp + '&appid=869e6fd2f6e579ae650d308cb7f62cb4', function(str) {
+     $.getJSON('https://api.openweathermap.org/data/2.5/weather?q=' + strTemp + '&appid=869e6fd2f6e579ae650d308cb7f62cb4', function(str) {
          console.log(str);
          /*optional stuff to do after success */
          $("#text2change").html("LOCATION:" + str.name + "</br>" + "Latitude:" + str.coord.lat + "</br>" + "Longitude:" + str.coord.lon + "</br>" + "TEMP:" + (str.main.temp - 273.15) + "</br>" + "</br>" + "MIN TEMP:" + (str.main.temp_min - 273.15) + "</br>" + "MAX TEMP:" + (str.main.temp_max - 273.15) + "</br>" + "PRESSURE:" + str.main.pressure + "</br>");
@@ -48,7 +48,7 @@
  $(document).ready(function() {
      if (navigator.geolocation) {
          navigator.geolocation.getCurrentPosition(function(position) {
-             $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&appid=869e6fd2f6e579ae650d308cb7f62cb4', function(str) {
+             $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&appid=869e6fd2f6e579ae650d308cb7f62cb4', function(str) {
                  /*optional stuff to do after success */
                  $("#text2change").html("LOCATION:" + str.name + "</br>" + "Latitude:" + str.coord.lat + "</br>" + "Longitude:" + str.coord.lon + "</br>" + "TEMP:" + (str.main.temp - 273.15) + "</br>" + "    MIN TEMP:" + (str.main.temp_min - 273.15) + "</br>" + "MAX TEMP:" + (str.main.temp_max - 273.15) + "</br>" + "PRESSURE:" + str.main.pressure + "</br>");
 
